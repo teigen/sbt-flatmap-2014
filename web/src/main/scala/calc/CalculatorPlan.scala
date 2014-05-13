d571f8b15a7c626a7862247d5edf002e48b2a73e
softprops <d.tangren@gmail.com>
2014-05-13 07:25:26 +0200

    default args

--- web/src/main/scala/calc/CalculatorPlan.scala
+++ web/src/main/scala/calc/CalculatorPlan.scala
@@ -6,16 +6,16 @@ import unfiltered.response.{ BadRequest, Ok, Html5 }
 
 class CalculatorPlan extends Plan{
   def intent = {
-    case GET(Path("/")) => page("", "")
+    case GET(Path("/")) => page()
 
     case POST(Path("/")) & Params(p) =>
       val q = p("q").headOption.getOrElse("")
       Calculator.calculate(q.split(' ')).fold(
         msg => BadRequest ~> page(msg, q),
-        i => Ok ~> page(q + " = " + i, ""))
+        i => Ok ~> page(q + " = " + i))
   }
 
-  def page(msg:String, q:String) = Html5(
+  def page(msg:String = "", q:String = "") = Html5(
     <h2>{msg}</h2>
     <form action="/" method="post">
       <input type="text" name="q" value={q}/>
